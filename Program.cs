92f389a0b995209608b6463fd1ed265bbdcfb6ed
Catalin <catalin.mariuc@student.usv.ro>
2022-03-19 17:31:58 +0200

    Functionalitatea de gasire a unei persoane din fisier

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Meniu;
 
 namespace Proiect
@@ -7,20 +8,30 @@ namespace Proiect
     {
         static void Main(string[] args)
         {
-            //Carte carte_test;
-            Persoana persoanaTastatura;
-            //Console.WriteLine(carte_test); - inca nu pot face asta, urmeazaa
+            //DECLARATII DATE
+            Persoana persoanaTastatura = null;
+            StocareDateFisier.StocareDateFisierCarti adminCarti = new StocareDateFisier.StocareDateFisierCarti(ConfigurationManager.AppSettings.Get("numeFisierCarti"));
+            StocareDateFisier.StocareDateFisierPersoane adminPersoane = new StocareDateFisier.StocareDateFisierPersoane(ConfigurationManager.AppSettings.Get("numeFisierPersoane"));
+            Int32 nrPersoane = 0;
+            Persoana[] persoane = adminPersoane.GetPersoane(out nrPersoane);
+
+            //CONFIG
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
             Console.SetWindowSize(90, 20);
+
+            //INFINITE LOOP
             while(true)
             {
+                //AFISARE MENIU, SELECTARE OPTIUNE
                 Console.Clear();
                 Meniu.Meniu.printMeniu();
                 char opt = Meniu.Meniu.alegeOptiune();
-                switch(opt)
+                Console.Clear();
+                switch (opt)
                 {
                     case 'C':
+                        //CITIREA DATELOR DE LA TASTATURA(LAB 3)
                         Console.Write("Nume = ");
                         String tempNume = Console.ReadLine().Trim();
                         Console.Write("Prenume = ");
@@ -30,20 +41,48 @@ namespace Proiect
                         Console.Write("Nr. Telefon = ");
                         String tempNrTel = Console.ReadLine().Trim();
                         Console.Write("Data nasterii (DD/MM/YYYY) = ");
-                        int[] tempDATA = Array.ConvertAll(Console.ReadLine().Trim().Split('/'), s => int.Parse(s));
+                        int[] tempDATA = Array.ConvertAll(Console.ReadLine().Trim().Split('/'), s => int.Parse(s));//converteste din string in int tot vectorul, pe viitor voi face un constructor separat
                         DateTime tempDob = new DateTime(tempDATA[2], tempDATA[1], tempDATA[0]);
                         persoanaTastatura = new Persoana(tempNume, tempPrenume, tempEmail, tempNrTel, tempDob);
                         break;
                     case 'S':
-                        //de completat
+                        //SALVAREA DATELOR INTRODUSE DE LA TASTATURA IN FISIER(LAB 3)
+                        adminPersoane.AddPersoana(persoanaTastatura);
                         break;
                     case 'A':
-                        //de completat
+                        //AFISAREA DATELOR DIN FISIER PE CONSOLA(LAB 3)
+                        persoane = adminPersoane.GetPersoane(out nrPersoane);
+                        for(Int32 i=0; i<nrPersoane; i++)
+                            Console.WriteLine(persoane[i].MyToString());
+                        Console.WriteLine("Apasati o tasta pentru a continua...");
+                        Console.ReadKey();
+                        break;
+                    case 'F':
+                        //GASIREA UNEI PERSOANE DUPA NUME SI PRENUME
+                        Console.WriteLine("Introduceti numele si prenumele persoanei cautate:");
+                        String[] numeSiPrenumeTemp = Console.ReadLine().Trim().Split();
+                        bool gasit = false;
+                        for(Int32 i=0; i<nrPersoane; i++)
+                        {
+                            String[] persCurenta = persoane[i].MyToString().Split(',');
+                            if (persCurenta[0] == numeSiPrenumeTemp[0] && persCurenta[1] == numeSiPrenumeTemp[1])
+                            {
+                                Console.WriteLine($"Persoana Gasita!\n{persoane[i].MyToString()}");
+                                i = nrPersoane+3;
+                                gasit = true;
+                            }
+                        }
+                        if (!gasit)
+                            Console.WriteLine("Persoana nu a fost gasita!");
+                        Console.WriteLine("Apasati o tasta pentru a continua...");
+                        Console.ReadKey();
                         break;
                     case 'X':
+                        //EXIT(LAB 3)
                         Environment.Exit(0);
                         break;
                     default:
+                        //IN CAZ DE ORICE
                         Console.WriteLine("Nu exista optiunea aleasa!");
                         break;
                 }
---
+++ StocareDateFisier.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * 
+ * ACEST FISIER REZOLVA TEMA LABORATORULUI 3
+ * 
+ */
+
+using Proiect;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocareDateFisier
+{
+    // CLASA PENTRU LUCRUL CU PERSOANE
+    public class StocareDateFisierPersoane
+    {
+        private String numeFisier;
+        private const int NR_MAX_PERSOANE = 100;
+        public StocareDateFisierPersoane(String numeFisierParam)
+        {
+            numeFisier = numeFisierParam;
+            Stream streamFisierText = File.Open(numeFisier, FileMode.OpenOrCreate);
+            streamFisierText.Close();
+        }
+        public void AddPersoana(Persoana pers)
+        {
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamWriterFisierText.WriteLine(pers.MyToString());
+            }
+        }
+        public Persoana[] GetPersoane(out int nrPersoane)
+        {
+            Persoana[] persoane = new Persoana[NR_MAX_PERSOANE];
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                nrPersoane = 0;
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    persoane[nrPersoane++] = new Persoana(linieFisier);
+                }
+            }
+            return persoane;
+        }
+    }
+
+    // CLASA PENTRU LUCRUL CU CARTI
+    public class StocareDateFisierCarti
+    {
+        private String numeFisier;
+        private const int NR_MAX_CARTI = 100;
+        public StocareDateFisierCarti(String numeFisierParam)
+        {
+            numeFisier = numeFisierParam;
+            Stream streamFisierText = File.Open(numeFisier, FileMode.OpenOrCreate);
+            streamFisierText.Close();
+        }
+        public void AddCarte(Carte carte)
+        {
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamWriterFisierText.WriteLine(carte.MyToString());
+            }
+        }
+        public Carte[] GetPersoane(out int nrCarti)
+        {
+            Carte[] carti = new Carte[NR_MAX_CARTI];
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                nrCarti = 0;
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    carti[nrCarti++] = new Carte(linieFisier);
+                }
+            }
+            return carti;
+        }
+    }
+}
+
